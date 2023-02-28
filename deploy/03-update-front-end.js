f2ed693d7c63b04937ab7e6bc21c94eb574e95e6
hlpangaa <hlpangaa@connect.ust.hk>
2023-02-28 17:18:53 +0800

    update FE abi

--- deploy/03-update-front-end.js
+++ deploy/03-update-front-end.js
@@ -28,27 +28,46 @@ async function updateAbi() {
         nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
     )
 
-    const basicNft = await ethers.getContract("BasicNft")
+    const eventFactory = await ethers.getContract("EventFactory")
     fs.writeFileSync(
-        `${frontEndAbiLocation}BasicNft.json`,
-        basicNft.interface.format(ethers.utils.FormatTypes.json)
+        `${frontEndAbiLocation}EventFactory.json`,
+        eventFactory.interface.format(ethers.utils.FormatTypes.json)
     )
     fs.writeFileSync(
-        `${frontEndAbiLocation2}BasicNft.json`,
-        basicNft.interface.format(ethers.utils.FormatTypes.json)
+        `${frontEndAbiLocation2}EventFactory.json`,
+        eventFactory.interface.format(ethers.utils.FormatTypes.json)
+    )
+
+    const eventContract = await ethers.getContract("EventContract")
+    fs.writeFileSync(
+        `${frontEndAbiLocation}EventContract.json`,
+        eventContract.interface.format(ethers.utils.FormatTypes.json)
+    )
+    fs.writeFileSync(
+        `${frontEndAbiLocation2}EventContract.json`,
+        eventContract.interface.format(ethers.utils.FormatTypes.json)
     )
 }
 
 async function updateContractAddresses() {
     const chainId = network.config.chainId.toString()
     const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const eventFactory = await ethers.getContract("EventFactory")
+
     const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
     if (chainId in contractAddresses) {
-        if (!contractAddresses[chainId]["NftMarketplace"].includes(nftMarketplace.address)) {
+        if (
+            !contractAddresses[chainId]["NftMarketplace"].includes(nftMarketplace.address) ||
+            !contractAddresses[chainId]["EventFactory"].includes(nftMarketplace.address)
+        ) {
             contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address)
+            contractAddresses[chainId]["EventFactory"].push(eventFactory.address)
         }
     } else {
-        contractAddresses[chainId] = { NftMarketplace: [nftMarketplace.address] }
+        contractAddresses[chainId] = {
+            NftMarketplace: [nftMarketplace.address],
+            EventFactory: [eventFactory.address],
+        }
     }
     fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
     fs.writeFileSync(frontEndContractsFile2, JSON.stringify(contractAddresses))
