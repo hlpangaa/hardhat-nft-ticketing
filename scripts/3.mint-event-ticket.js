eeade7bc763ff8cec7bb159b0b7b6a7595e7b573
hlpangaa <hlpangaa@connect.ust.hk>
2023-02-28 23:02:42 +0800

    20230228 mint script update

--- scripts/3.mint-event-ticket.js
+++ scripts/3.mint-event-ticket.js
@@ -7,9 +7,9 @@ async function main() {
     if (network.config.chainId == "31337") {
         to = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
     } else {
-        // to = "0x0A270fB0CEa1cCB113860B0Af6CbB98c1a0c04C8" // goerli account 6
+        to = "0x0A270fB0CEa1cCB113860B0Af6CbB98c1a0c04C8" // goerli account 6
         //to = "0xf06EbE55f79bd62380e73bb040f6E954a824957e" // goerli account 2
-        to = "0x29B3555cE30A36F042Fb864866CE35ba2b63F25F" // goerli account 1
+        // to = "0x29B3555cE30A36F042Fb864866CE35ba2b63F25F" // goerli account 1
     }
     //const _mintFee = ethers.utils.parseEther("0.1")
 
